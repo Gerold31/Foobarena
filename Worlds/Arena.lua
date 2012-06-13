e0c690af30e24770566118e10b52d43cee3c6ab9
Gerold <kramer.leroy@gmail.com>
2012-06-13 17:33:08 +0200

    Improve mapscript

--- Worlds/Arena.lua
+++ Worlds/Arena.lua
@@ -1,25 +1,43 @@
+
 wait=coroutine.yield;
 Console.Print("Arena.lua script loaded.\n");
 
+maxRobots = 4;
+
 function Game()
-	wait(5);
+	wait(1);
 	
 	local timer = 5;
 	
+	local round = 0;
+	
+	local robotsPerRound = 2
+	local robotsToSpawn = 0;
+	
 	while true do
-		if countRobots() == 0 then
+		if countRobots() == 0 and robotsToSpawn == 0 then
 			if timer > 0 then
-				--GUI
+				Player1:printHUD("Round " .. round+1 .. " starts in " .. timer);
+				Player1:giveAmmo(200);
+				timer = timer - 1;
+				
+			elseif timer == 0 then
+				round = round + 1;
+				timer = timer - 1;
+				robotsToSpawn = robotsPerRound * round;
+							
+			else 
+				timer = 5;
+								
 			end
-			-- spawn robots
-			spawnRobot1();
-			wait(0.1);
-			spawnRobot2();
-			wait(0.1);
-			spawnRobot3();
-			wait(0.1);
-			spawnRobot4();
 		else
+			Player1:printHUD("Round " .. round .. ": " .. robotsToSpawn + countRobots() .. " robots left");
+			
+			
+			local robots = math.min(maxRobots - countRobots(), robotsToSpawn);
+			Console.Print("min(" .. maxRobots - countRobots() .. ", " .. robotsToSpawn .. ")==" .. robots .. "\n");
+			spawnRobots(robots);
+			robotsToSpawn = robotsToSpawn - robots;
 		end
 		wait(1);
 	end
@@ -37,28 +55,23 @@ function countRobots()
 	return robots
 end
 
-function countEntities()
-	local entities = 0
-	for k, v in pairs(_G) do                      -- Iterate over all global variables.
-		if type(v)=="table" and v.GetName then    -- Implement a very simple type check.
-			entities=entities+1
-		end
-	end
-	Console.Print("entities: " .. entities .. "\n");
-	return entities
+function spawnRobots(robots)	
+	for i=1, robots, 1 do
+		spawnRobot(math.random(4));
+		wait(0.1);
+	end	
 end
 
-function spawnRobot1()
-	RobotSpawner_001:spawnRobot(-1, -1, -1, -1);
-end
-function spawnRobot2()
-	RobotSpawner_002:spawnRobot(-1, -1, -1, -1);
-end
-function spawnRobot3()
-	RobotSpawner_003:spawnRobot(-1, -1, -1, -1);
-end
-function spawnRobot4()
-	RobotSpawner_004:spawnRobot(-1, -1, -1, -1);
+function spawnRobot(i)
+	if i == 1 then
+		RobotSpawner_001:spawnRobot(-1, -1, -1, -1);
+	elseif i == 2 then
+		RobotSpawner_002:spawnRobot(-1, -1, -1, -1);
+	elseif i == 3 then
+		RobotSpawner_003:spawnRobot(-1, -1, -1, -1);
+	else
+		RobotSpawner_004:spawnRobot(-1, -1, -1, -1);
+	end
 end
 
 
